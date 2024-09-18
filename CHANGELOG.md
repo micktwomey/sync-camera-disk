bbd22324261d23c84227971d262cdc5c3e23be6c
Michael Twomey <mick@twomeylee.name>
2024-09-18 22:49:49 +0100

    Fix handling of nested folders in source
    Was flattening structures down (which is fine for most cameras), which breaks the ATEM ISO project structure.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+- Fix handling of sub folders when copying files (in particular the ATEM ISO structure). Was flattening the folder structures.
 
 ## [0.7.0] - 2024-09-18
 ### Added
--- tests/test_destination.py
+++ tests/test_destination.py
@@ -29,17 +29,17 @@ def test_is_file_identical(tmp_path: Path) -> None:
 def test_dated_folder_destination(tmp_path: Path) -> None:
     dest = destination.DatedFolderDestination(prefix=tmp_path / "destination")
 
-    file1 = File(path=tmp_path / "source" / "abc.mp4")
+    file1 = File(path=tmp_path / "abc.mp4")
     file1.path.parent.mkdir(parents=True, exist_ok=True)
     file1.path.touch()
-    file2 = File(path=tmp_path / "source" / "abc.srt")
+    file2 = File(path=tmp_path / "abc.srt")
     file2.path.parent.mkdir(parents=True, exist_ok=True)
     file2.path.touch()
-    file3 = File(path=tmp_path / "source" / "abc.txt")
+    file3 = File(path=tmp_path / "abc.txt")
     file3.path.parent.mkdir(parents=True, exist_ok=True)
     with file3.path.open("w") as fp:
         fp.write("hello")
-    file4 = File(path=tmp_path / "source" / "abc.text")
+    file4 = File(path=tmp_path / "abc.text")
     file4.path.parent.mkdir(parents=True, exist_ok=True)
     with file4.path.open("w") as fp:
         fp.write("ham")
@@ -47,13 +47,13 @@ def test_dated_folder_destination(tmp_path: Path) -> None:
     today = datetime.date.today()
     date = today.isoformat()
 
-    p = tmp_path / "destination" / date / "prefix" / "abc.txt"
+    p = tmp_path / "destination" / date / "abc.txt"
     p.parent.mkdir(exist_ok=True, parents=True)
     p.touch()
 
     assert not destination.is_file_identical(file3.path, p)
 
-    p = tmp_path / "destination" / date / "prefix" / "abc.text"
+    p = tmp_path / "destination" / date / "abc.text"
     p.parent.mkdir(exist_ok=True, parents=True)
     with p.open("w") as fp:
         fp.write("ham")
@@ -65,32 +65,65 @@ def test_dated_folder_destination(tmp_path: Path) -> None:
             file_set=FileSet(
                 files=[file1, file2, file3, file4],
                 stem="abc",
-                prefix=Path("prefix"),
-                volume_path=tmp_path / "source",
+                prefix=Path(""),
+                volume_path=tmp_path,
                 volume_identifier="abc",
             )
         )
     )
 
-    assert operations == [
+    assert [o.dict() for o in operations] == [
+        o.dict()
+        for o in (
+            Operation(
+                operation=OperationType.copy,
+                source=file1.path,
+                destination=tmp_path / "destination" / date / "abc.mp4",
+            ),
+            Operation(
+                operation=OperationType.copy,
+                source=file2.path,
+                destination=tmp_path / "destination" / date / "abc.srt",
+            ),
+            Operation(
+                operation=OperationType.unknown,
+                source=file3.path,
+                destination=tmp_path / "destination" / date / "abc.txt",
+            ),
+            Operation(
+                operation=OperationType.identical,
+                source=file4.path,
+                destination=tmp_path / "destination" / date / "abc.text",
+            ),
+        )
+    ]
+
+
+def test_nested_folder_operation(tmp_path: Path) -> None:
+    """Ensure we generate correct operations for a nested folder structure"""
+    dest = destination.DatedFolderDestination(prefix=tmp_path / "destination")
+    today = datetime.date.today()
+    date = today.isoformat()
+
+    file1 = File(path=tmp_path / "project/Video Files/abc.mp4")
+    file1.path.parent.mkdir(parents=True, exist_ok=True)
+    file1.path.touch()
+    operations = list(
+        dest.generate_operations(
+            file_set=FileSet(
+                files=[file1],
+                stem="project",
+                prefix=Path("project"),
+                volume_path=tmp_path,
+                volume_identifier="abc",
+            )
+        )
+    )
+
+    assert [o.dict() for o in operations] == [
         Operation(
             operation=OperationType.copy,
             source=file1.path,
-            destination=tmp_path / "destination" / date / "prefix" / "abc.mp4",
-        ),
-        Operation(
-            operation=OperationType.copy,
-            source=file2.path,
-            destination=tmp_path / "destination" / date / "prefix" / "abc.srt",
-        ),
-        Operation(
-            operation=OperationType.unknown,
-            source=file3.path,
-            destination=tmp_path / "destination" / date / "prefix" / "abc.txt",
-        ),
-        Operation(
-            operation=OperationType.identical,
-            source=file4.path,
-            destination=tmp_path / "destination" / date / "prefix" / "abc.text",
-        ),
+            destination=tmp_path / "destination" / date / "project/Video Files/abc.mp4",
+        ).dict(),
     ]
